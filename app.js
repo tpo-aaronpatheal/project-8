51591388384f76bcbdc3b44452dd596630b12324
Erica Collier <ercollie@travelers.com>
2021-03-15 12:21:56 -0400

    up to part 5, working

--- app.js
+++ app.js
@@ -1,39 +1,46 @@
-//require sequilize and use constructor to connect to database. Sequilize now holds the instance that can be interacted with. 
+// require sequilize and use constructor to connect to database. Sequilize now holds the instance that can be interacted with.
 
-//require/import sequelize from index.js
+// require/import sequelize from index.js
 const db = require('./models');
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
 
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// tests connection and syncs syncs the model with the database.
+(async () => {
+  await db.sequelize.sync();
+  try {
+    await db.sequelize.authenticate();
+    console.log('Connection to the database successful!');
+  } catch (error) {
+    console.error('Error connecting to the database: ', error);
+  }
+})();
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -43,17 +50,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-
-//tests connection and syncs syncs the model with the database. 
-(async () => {
-  await db.sequelize.sync();
-  try {
-      await db.sequelize.authenticate();
-      console.log('Connection to the database successful!');
-  } catch(error) {
-      console.error('Error connecting to the database: ', error);
-  }
-})();
-
 module.exports = app;
-
--- models/book.js
+++ models/book.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Book extends Model {
     /**
@@ -9,20 +7,20 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate (models) {
       // define association here
     }
-  };
+  }
   Book.init({
     title: {
       type: DataTypes.STRING,
-      allowNull: false, 
+      allowNull: false,
       validate: {
         notNull: {
-            msg:'Please provide a value for "Title"',
+          msg: 'Please provide a value for "Title"'
         },
         notEmpty: {
-            msg: 'Please provide a value for "Title"',
+          msg: 'Please provide a value for "Title"'
         }
       }
     },
@@ -31,18 +29,18 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       validate: {
         notNull: {
-            msg:'Please provide a value for "Author"',
+          msg: 'Please provide a value for "Author"'
         },
         notEmpty: {
-            msg: 'Please provide a value for "Author"',
+          msg: 'Please provide a value for "Author"'
         }
-      } 
+      }
     },
     genre: DataTypes.STRING,
     year: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'Book',
+    modelName: 'Book'
   });
   return Book;
 };
