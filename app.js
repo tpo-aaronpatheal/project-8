f95e43246639695c5e3b31e824140cdd2c8f744e
tpo-tstoll <77018670+tpo-tstoll@users.noreply.github.com>
2021-03-15 15:43:25 -0500

    Pug updates index/layout

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'pug');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static('public'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
