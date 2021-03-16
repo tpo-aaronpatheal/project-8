21e75e837aaa97cbe9557bb3c303752a269b49f7
tpo-apatheal <77015964+tpo-apatheal@users.noreply.github.com>
2021-03-16 09:32:11 -0700

    update book/:id

--- routes/books.js
+++ routes/books.js
@@ -17,13 +17,25 @@ function asyncHandler (cb) {
 /* GET home page. */
 router.get('/', asyncHandler(async (req, res) => {
   const books = await Book.findAll();
-  const { title, author, genre, year } = Book;
-  res.render('index', {books, title, author, genre, year});
+  res.render('index', {books});
 }));
 
+/* GET new book*/
 router.get('/new', (req, res) => {
   res.render('new-book', { book: {} });
 });
+
+/*Get individual book */
+router.get('/:id', asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  console.log(book);
+  if(book) {
+    res.render("update-book", { book } ) ;
+  } else {
+    res.sendStatus(404);
+  }
+}))
+
 
 router.post('/', asyncHandler(async (req, res) => {
   let book;
