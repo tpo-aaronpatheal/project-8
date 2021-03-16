2fc73b1e527fd226ef8e66cd8bbd42a6233e10fa
tpo-nwuerz <77018017+tpo-nwuerz@users.noreply.github.com>
2021-03-16 12:15:30 -0500

    adds delete and update routes

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { Book } = require('../models');
 
 /* Handler function to wrap each route. */
-function asyncHandler (cb) {
+function asyncHandler(cb) {
   return async (req, res, next) => {
     try {
       await cb(req, res, next);
@@ -17,7 +17,7 @@ function asyncHandler (cb) {
 /* GET home page. */
 router.get('/', asyncHandler(async (req, res) => {
   const books = await Book.findAll();
-  res.render('index', {books});
+  res.render('index', { books });
 }));
 
 /* GET new book*/
@@ -29,13 +29,44 @@ router.get('/new', (req, res) => {
 router.get('/:id', asyncHandler(async (req, res) => {
   const book = await Book.findByPk(req.params.id);
   console.log(book);
-  if(book) {
-    res.render("update-book", { book } ) ;
+  if (book) {
+    res.render("update-book", { book });
   } else {
     res.sendStatus(404);
   }
-}))
+}));
 
+router.post('/:id/update', asyncHandler(async (req, res) => {
+  let book;
+  try {
+    book = await Book.findByPk(req.params.id);
+    if (book) {
+      await book.update(req.body);
+      res.redirect('/books/' + book.id)
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    if (error.name === "SequelizeValidationError") {
+      book = await Book.build(req.body);
+      book.id = req.params.id;
+      res.render("update-book", { book, error: error.errors })
+    } else {
+      throw error;
+    }
+  }
+}));
+
+router.post('/:id/delete', asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  if (book) {
+    await book.destroy();
+    res.redirect('/books');
+  } else {
+    res.sendStatus(404);
+  }
+}
+))
 
 router.post('/', asyncHandler(async (req, res) => {
   let book;
@@ -45,7 +76,7 @@ router.post('/', asyncHandler(async (req, res) => {
   } catch (error) {
     if (error.name === 'SequelizeValidationError') {
       book = await Book.build(req.body);
-      res.render('error', { book, errors: error.errors, title: 'New Book' });
+      res.render('new-book', { book, errors: error.errors });
     } else {
       throw error;
     }
